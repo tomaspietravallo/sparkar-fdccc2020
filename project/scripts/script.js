b9225e48e44da6160fc73ee663918a62f2094a03
Tomas Pietravallo <62444821+tomaspietravallo@users.noreply.github.com>
2020-11-28 13:06:26 -0300

    Implement feedback

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -1,33 +1,62 @@
 const Scene = require('Scene');
 const FaceTracking = require('FaceTracking');
 const Reactive = require('Reactive');
+const Materials = require('Materials');
 
-async function animateMeshes(){
-  let meshes = await Scene.root.findByPath('**/meshes0/*');
+async function createAndAnimateMeshes() {
+  // Settings you can modfify (download the project play around! 🙌✨)
+  let amountOfMeshes = 36;
+  let maxSmooth = 350;
+  let maxDelay = 0.5;
+  // ----------------
+
+  let meshesParent = await Scene.root.findFirst('meshesNullObj');
+
+  let materials = await Materials.findUsingPattern('mask-material*');
+
+  materials.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
 
   let transform = FaceTracking.face(0).cameraTransform;
 
-  let maxSmooth = 350;
-  let maxDelay = 0.5;
+  for (let index = 0; index < amountOfMeshes; index++) {
+    let newMesh = await Scene.create('FaceMesh', {
+      name: 'mesh' + index,
+    });
 
-  for (let index = 0; index < meshes.length; index++) {
-      let mesh = meshes[index];
+    await meshesParent.addChild(newMesh);
 
-      let smoothBy = (maxSmooth / meshes.length) * (index + 1);
-      let delayBy = (maxDelay / meshes.length) * (index + 1);
+    let materialIndex = Math.round(
+      (index / amountOfMeshes) * (materials.length - 1)
+    );
 
-      let xValue = Reactive.expSmooth( transform.x.delayBy({ milliseconds: delayBy }), smoothBy);
-      let yValue = Reactive.expSmooth( transform.y.delayBy({ milliseconds: delayBy }), smoothBy);
-      let zValue = Reactive.expSmooth( transform.z.delayBy({ milliseconds: delayBy * 10 }), smoothBy);
+    newMesh.material = materials[materialIndex];
 
-      mesh.transform.x = xValue;
-      mesh.transform.y = yValue;
-      mesh.transform.z = zValue;
+    let smoothBy = (maxSmooth / amountOfMeshes) * (index + 1);
+    let delayBy = (maxDelay / amountOfMeshes) * (index + 1);
 
-      mesh.transform.rotationX = transform.rotationX;
-      mesh.transform.rotationY = transform.rotationY;
-      mesh.transform.rotationZ = transform.rotationZ;
+    let xValue = Reactive.expSmooth(
+      transform.x.delayBy({ milliseconds: delayBy }),
+      smoothBy
+    );
+    let yValue = Reactive.expSmooth(
+      transform.y.delayBy({ milliseconds: delayBy }),
+      smoothBy
+    );
+    let zValue = Reactive.expSmooth(
+      transform.z.delayBy({ milliseconds: delayBy * 10 }),
+      smoothBy
+    );
+
+    newMesh.transform.x = xValue;
+    newMesh.transform.y = yValue;
+    newMesh.transform.z = zValue;
+
+    newMesh.transform.rotationX = transform.rotationX;
+    newMesh.transform.rotationY = transform.rotationY;
+    newMesh.transform.rotationZ = transform.rotationZ;
   }
-};
+}
 
-animateMeshes();
+createAndAnimateMeshes();
